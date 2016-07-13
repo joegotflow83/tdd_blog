c71f877e5f0c910c6c179fc92b924886aa875a43
joegotflow83 <joe@absolutod.com>
2016-07-13 14:44:30 -0400

    First test to pass testing models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-
+class Post(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=32)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Post
+
+
+# Models Test
+class MainTest(TestCase):
+    """Test models in main app"""
+    def test_post_models(self):
+        user = User.objects.create(username="joe")
+        post = Post.objects.create(user=user, title="TDD", body="I am practicing tdd")
+        self.assertTrue(isinstance(post, Post))
+
